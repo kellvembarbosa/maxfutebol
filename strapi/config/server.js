be5b4745d36c2cbbf284ed9d21653086e835c284
Kellvem Barbosa <kellvembarbosa@icloud.com>
2020-10-10 20:23:39 +0100

    principal

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '78430aa5ca3ff7593b03fd8e60aefde9'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a6d89ece-e605-4726-b889-384d47fb50c7'
+};
